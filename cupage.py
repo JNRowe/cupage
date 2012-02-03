5d9b4361bfac52e8aca3715baa85f78c7f66c052
James Rowe <jnrowe@gmail.com>
2012-02-03 11:35:23 +0000

    Added simple runner to use cupage from source repository.

---
+++ cupage.py
@@ -0,0 +1,24 @@
+#! /usr/bin/python -tt
+"""cupage - a tool to check for updates on web pages"""
+# Copyright (C) 2010-2012  James Rowe <jnrowe@gmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+import sys
+
+from libcupage import commandline
+
+
+sys.exit(commandline.main())
