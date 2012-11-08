3bb14d06cce9b1b51eaee21a47b6c64151cc5b2e
James Rowe <jnrowe@gmail.com>
2012-11-08 20:01:38 +0000

    Moved doctests to separate files.

--- cupage/__init__.py
+++ cupage/__init__.py
@@ -270,28 +270,7 @@ class Site(object):
 
     @staticmethod
     def package_re(name, ext, verbose=False):
-        r"""Generate a compiled ``re`` for the package
-
-        >>> c = Site.package_re("test", "tar")
-        >>> re.match(c, "test-0.1.2.tar.bz2").group()
-        'test-0.1.2.tar.bz2'
-        >>> re.match(c, "test-0.1.2_rc2.tar.gz").group()
-        'test-0.1.2_rc2.tar.gz'
-        >>> c.pattern
-        'test-[\\d\\.]+(?:[_-](?:pre|rc)[\\d]+)?\\.tar.(?:bz2|gz)'
-        >>> c = Site.package_re("test", "zip")
-        >>> re.match(c, "test-0.1.2-rc2.zip").group()
-        'test-0.1.2-rc2.zip'
-        >>> re.match(c, "test-0.1.2-pre2.zip").group()
-        'test-0.1.2-pre2.zip'
-        >>> c.pattern
-        'test-[\\d\\.]+(?:[_-](?:pre|rc)[\\d]+)?\\.zip'
-        >>> c = Site.package_re("test_long", "gem")
-        >>> re.match(c, "test_long-0.1.2.gem").group()
-        'test_long-0.1.2.gem'
-        >>> c.pattern
-        'test_long-[\\d\\.]+(?:[_-](?:pre|rc)[\\d]+)?\\.gem'
-        """
+        """Generate a compiled ``re`` for the package"""
         if ext == "tar":
             ext = "tar.(?:bz2|gz)"
         match = r"%s-[\d\.]+(?:[_-](?:pre|rc)[\d]+)?\.%s" % (name, ext)
