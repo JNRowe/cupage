5a94fa98a4a7a133baaba632215324d8b9c90624
James Rowe <jnrowe@gmail.com>
2012-11-09 19:53:23 +0000

    Simplify tests using nose2's params plugin.
    This removes a heap of duplication in the test code.

--- tests/test___init__.py
+++ tests/test___init__.py
@@ -1,26 +1,22 @@
 import re
 
 from expecter import expect
+from nose2.tools import params
 
 from cupage import Site
 
 
-def test_package_re():
-    c = Site.package_re("test", "tar")
-    expect(re.match(c, "test-0.1.2.tar.bz2").group()) == 'test-0.1.2.tar.bz2'
-    expect(re.match(c, "test-0.1.2_rc2.tar.gz").group()) == \
-        'test-0.1.2_rc2.tar.gz'
-    expect(c.pattern) == \
-        'test-[\\d\\.]+(?:[_-](?:pre|rc)[\\d]+)?\\.tar.(?:bz2|gz)'
+@params(
+    ("test", "tar", ["test-0.1.2.tar.bz2", "test-0.1.2_rc2.tar.gz"],
+     'test-[\\d\\.]+(?:[_-](?:pre|rc)[\\d]+)?\\.tar.(?:bz2|gz)'),
 
-    c = Site.package_re("test", "zip")
-    expect(re.match(c, "test-0.1.2-rc2.zip").group()) == \
-        'test-0.1.2-rc2.zip'
-    expect(re.match(c, "test-0.1.2-pre2.zip").group()) == \
-        'test-0.1.2-pre2.zip'
-    expect(c.pattern) == 'test-[\\d\\.]+(?:[_-](?:pre|rc)[\\d]+)?\\.zip'
-
-    c = Site.package_re("test_long", "gem")
-    expect(re.match(c, "test_long-0.1.2.gem").group()) == \
-        'test_long-0.1.2.gem'
-    expect(c.pattern) == 'test_long-[\\d\\.]+(?:[_-](?:pre|rc)[\\d]+)?\\.gem'
+    ("test", "zip", ["test-0.1.2-rc2.zip", "test-0.1.2-pre2.zip"],
+     'test-[\\d\\.]+(?:[_-](?:pre|rc)[\\d]+)?\\.zip'),
+    ("test_long", "gem", ["test_long-0.1.2.gem", ],
+     'test_long-[\\d\\.]+(?:[_-](?:pre|rc)[\\d]+)?\\.gem'),
+)
+def test_package_re(name, ext, pkgs, pattern):
+    c = Site.package_re(name, ext)
+    for pkg in pkgs:
+        expect(re.match(c, pkg).group()) == pkg
+    expect(c.pattern) == pattern
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,22 +1,31 @@
 import datetime
 
 from expecter import expect
+from nose2.tools import params
 
 from cupage.utils import (parse_timedelta, sort_packages)
 
 
-def test_parse_timedelta():
-    expect(parse_timedelta("3h")) == datetime.timedelta(0, 10800)
-    expect(parse_timedelta("1d")) == datetime.timedelta(1)
-    expect(parse_timedelta("1 d")) == datetime.timedelta(1)
-    expect(parse_timedelta("0.5 y")) == datetime.timedelta(182, 43200)
-    expect(parse_timedelta("0.5 Y")) == datetime.timedelta(182, 43200)
+@params(
+    ("3h", datetime.timedelta(0, 10800)),
+    ("1d", datetime.timedelta(1)),
+    ("1 d", datetime.timedelta(1)),
+    ("0.5 y", datetime.timedelta(182, 43200)),
+    ("0.5 Y", datetime.timedelta(182, 43200)),
+)
+def test_parse_timedelta(string, dt):
+    expect(parse_timedelta(string)) == dt
+
+
+def test_parse_invalid_timedelta():
     with expect.raises(ValueError, "Invalid 'frequency' value"):
         parse_timedelta("1 k")
 
 
-def test_sort_packages():
-    expect(sort_packages(['pkg-0.1.tar.gz', 'pkg-0.2.1.tar.gz', 'pkg-0.2.tar.gz'])) == \
-        ['pkg-0.1.tar.gz', 'pkg-0.2.tar.gz', 'pkg-0.2.1.tar.gz']
-    expect(sort_packages(['v0.1.0', 'v0.11.0', 'v0.1.2'])) == \
-        ['v0.1.0', 'v0.1.2', 'v0.11.0']
+@params(
+    (['pkg-0.1.tar.gz', 'pkg-0.2.1.tar.gz', 'pkg-0.2.tar.gz'],
+     ['pkg-0.1.tar.gz', 'pkg-0.2.tar.gz', 'pkg-0.2.1.tar.gz']),
+    (['v0.1.0', 'v0.11.0', 'v0.1.2'], ['v0.1.0', 'v0.1.2', 'v0.11.0']),
+)
+def test_sort_packages(input, ordered):
+    expect(sort_packages(input)) == ordered
