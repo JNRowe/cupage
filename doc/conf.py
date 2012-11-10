584a9f8c19a799b80ab12c30ced54b7ba16c7f2d
James Rowe <jnrowe@gmail.com>
2012-11-10 21:04:22 +0000

    Enabled Sphinx's testing extensions.

--- doc/conf.py
+++ doc/conf.py
@@ -28,7 +28,8 @@ import sys
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = \
-    ["sphinx.ext.%s" % ext for ext in ["autodoc", "intersphinx", "viewcode"]] + \
+    ["sphinx.ext.%s" % ext for ext in ["autodoc", "coverage", "doctest",
+                                       "intersphinx", "viewcode"]] + \
     ["sphinxcontrib.%s" % ext for ext in ["blockdiag", ]]
 
 # Add any paths that contain templates here, relative to this directory.
