e1a412b9a462c8520b153d1c5aa1d09a0b2d5ad2
James Rowe <jnrowe@gmail.com>
2012-11-10 21:00:40 +0000

    Enabled the Sphinx intersphinx externsion.

--- doc/conf.py
+++ doc/conf.py
@@ -28,7 +28,7 @@ import sys
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = \
-    ["sphinx.ext.%s" % ext for ext in ["autodoc", ]] + \
+    ["sphinx.ext.%s" % ext for ext in ["autodoc", "intersphinx"]] + \
     ["sphinxcontrib.%s" % ext for ext in ["blockdiag", ]]
 
 # Add any paths that contain templates here, relative to this directory.
