c4f589fd1885ad7f540245579fd2dcd59eb8321d
James Rowe <jnrowe@gmail.com>
2013-09-01 05:38:05 +0100

    0.8.2 released

--- cupage/_version.py
+++ cupage/_version.py
@@ -1,9 +1,9 @@
-# This is cupage version 0.8.1 (2013-08-19)
+# This is cupage version 0.8.2 (2013-09-01)
 # pylint: disable=C0103, C0111, C0121, W0622
 
-dotted = '0.8.1'
-libtool = '8:21'
-hex = 0x000801
-date = '2013-08-19'
-tuple = (0, 8, 1)
-web = 'cupage/0.8.1'
+dotted = '0.8.2'
+libtool = '8:22'
+hex = 0x000802
+date = '2013-09-01'
+tuple = (0, 8, 2)
+web = 'cupage/0.8.2'
